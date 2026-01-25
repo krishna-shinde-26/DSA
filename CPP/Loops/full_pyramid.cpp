0dedc7044e422eca64c819a91bbcb4164223f0f5
krishna-shinde-26 <krishnashindeoffcial@gmail.com>
2026-01-25 16:42:28 +0530

    full pyamid

--- CPP/Loops/full_pyramid.cpp
+++ CPP/Loops/full_pyramid.cpp
@@ -65,3 +65,104 @@ cout<<endl;
 }
 
 
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    
+               int n =9 ;
+    
+           for(int i = 1 ; i <n;i++){
+               for(int j=0;j<n-i-1;j++){
+                   cout<<" ";
+               }
+                   for(int k=1;k<i;k++){
+                       
+                       cout<<"* ";
+                       
+                   }
+                cout<<endl;
+               }
+               
+               
+               
+           
+    
+    
+}
+
+/*
+output 
+#include<iostream>
+using namespace std;
+int main(){
+    
+               int n =9 ;
+    
+           for(int i = 1 ; i <n;i++){
+               for(int j=0;j<n-i-1;j++){
+                   cout<<" ";
+               }
+                   for(int k=1;k<i;k++){
+                       
+                       cout<<"* ";
+                       
+                   }
+                cout<<endl;
+               }
+               
+               
+               
+           
+    
+    
+}
+
+/*
+output 
+[Running] cd "/home/krishna/Coding area/DSA/CPP/Loops/" && g++ Triangler_aryramid.cpp -o Triangler_aryramid && "/home/krishna/Coding area/DSA/CPP/Loops/"Triangler_aryramid
+ #include<iostream>
+using namespace std;
+int main(){
+    
+               int n =9 ;
+    
+           for(int i = 1 ; i <n;i++){
+               for(int j=0;j<n-i-1;j++){
+                   cout<<" ";
+               }
+                   for(int k=1;k<i;k++){
+                       
+                       cout<<"* ";
+                       
+                   }
+                cout<<endl;
+               }
+               
+               
+               
+           
+    
+    
+}
+
+/*
+output 
+[Running] cd "/home/krishna/Coding area/DSA/CPP/Loops/" && g++ Triangler_aryramid.cpp -o Triangler_aryramid && "/home/krishna/Coding area/DSA/CPP/Loops/"Triangler_aryramid
+       
+      * 
+     * * 
+    * * * 
+   * * * * 
+  * * * * * 
+ * * * * * * 
+* * * * * * * 
+* * * * * * * * 
+ * * * * * * * 
+  * * * * * * 
+   * * * * * 
+    * * * * 
+     * * * 
+      * * 
+       * 
+*/
