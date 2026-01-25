2584781185e16d15aa77e12ced7cfed03a625ee0
krishna-shinde-26 <krishnashindeoffcial@gmail.com>
2026-01-25 14:59:52 +0530

    number pyamid

---
+++ CPP/Loops/number_pyamid.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+for(int i = 0 ; i<6;i++)
+{
+
+for(int j = 1 ;j<i+1;j++){
+
+
+cout<<j;
+
+
+}
+cout<<endl;
+}
+
+
+
+}
+
+/*
+output 
+
+[Running] cd "/home/krishna/Coding area/DSA/CPP/Loops/" && g++ number_pyamid.cpp -o number_pyamid && "/home/krishna/Coding area/DSA/CPP/Loops/"number_pyamid
+
+1
+12
+123
+1234
+12345
+
+
+*/
