bd11ec3338523d416a0e15904b0ffe432024e2ca
krishna-shinde-26 <krishnashindeoffcial@gmail.com>
2026-01-25 14:50:31 +0530

    add

---
+++ CPP/Loops/revarse_pyramid_pattern.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+using namespace std;
+
+
+
+int main(){
+
+    for(int i=9 ;i>=0;i--){
+    
+  for(int j=1;j<i  ;j++){
+
+cout<<"*";
+
+}
+
+cout<<endl;
+        
+}
+    return 0;
+    
+}
+/*
+
+********
+*******
+******
+*****
+****
+***
+**
+*
+
+
+
+
+=== Code Execution Successful === 
+*/
