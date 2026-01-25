eb6476a4126fdf946f1692de23920cb4e4c4558f
krishna-shinde-26 <krishnashindeoffcial@gmail.com>
2026-01-25 15:05:58 +0530

    revers_number pyramid

---
+++ CPP/Loops/revers_number_pyamid.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+for(int i = 9 ; i>0;i--){
+
+for(int j = 1 ; j<i;j++){
+
+cout<<j;
+
+}
+
+cout<<endl;
+}
+
+
+
+}
+
+
+/*
+output 
+
+[Running] cd "/home/krishna/Coding area/DSA/CPP/Loops/" && g++ revers_number_pyamid.cpp -o revers_number_pyamid && "/home/krishna/Coding area/DSA/CPP/Loops/"revers_number_pyamid
+12345678
+1234567
+123456
+12345
+1234
+123
+12
+1*/
