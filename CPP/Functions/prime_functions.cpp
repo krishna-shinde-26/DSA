0172cc631229e5f7b9ff9a9c743449c55958865d
krishna-shinde-26 <krishnashindeoffcial@gmail.com>
2026-01-26 12:28:54 +0530

    function two

---
+++ CPP/Functions/prime_functions.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+void prime(int a){
+
+
+if(a%2==1){
+
+cout<<"your input is prime number : "<<a ;
+
+}
+else {
+
+cout<<"not a prime number";
+
+}
+
+
+
+
+
+}
+
+
+int main(){
+
+prime(1);
+
+
+}
+
+
