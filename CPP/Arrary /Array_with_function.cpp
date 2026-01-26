bd164978ee1284c66c68d1c2b9e6f4c2debe9af1
krishna-shinde-26 <krishnashindeoffcial@gmail.com>
2026-01-26 17:15:18 +0530

    array with function is done

---
+++ CPP/Arrary /Array_with_function.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+
+
+using namespace std;
+ 
+void solve(int arr[],int size){
+    
+    
+    for(int i = 0;i<size;i++){
+          
+        cout<<"arr index is : "<<i<<" : "<<arr[i]<<endl;
+
+    }}
+    
+        int main(){
+
+
+            int size = 5 ;
+            int arr[size];
+            for(int i = 0;i<size;i++){
+               
+                cin>>arr[i];
+             
+
+    }
+
+           // always remember you need to always pass size and arr name in the function 
+            solve(arr,size);
+
+
+
+        
+
+
+
+}
+
+
+/* 
+2 4 5 5 6 
+arr index is : 0 : 2
+arr index is : 1 : 4
+arr index is : 2 : 5
+arr index is : 3 : 5
+arr index is : 4 : 6
+
+
+=== Code Execution Successful === */
