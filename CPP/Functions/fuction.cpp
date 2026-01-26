d9f8108066ae72733e3f388f925b950b17ad3bfe
krishna-shinde-26 <krishnashindeoffcial@gmail.com>
2026-01-26 12:16:37 +0530

    starting with Functions

---
+++ CPP/Functions/fuction.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+
+int sum(int a ,int b ){
+
+      
+     int sum = a+b;
+
+       return sum;
+
+
+}
+
+
+int main(){
+
+ 
+    int total = sum(5,9);
+    cout<<"your same is: "<<total<<endl;
+
+
+
+
+}
+
+
