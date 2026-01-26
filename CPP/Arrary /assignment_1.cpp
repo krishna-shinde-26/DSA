82dd9caa430f8b60edf845419f7c135fa51684af
krishna-shinde-26 <krishnashindeoffcial@gmail.com>
2026-01-26 16:28:17 +0530

    assignment 1 done

---
+++ CPP/Arrary /assignment_1.cpp
@@ -0,0 +1,38 @@
+        #include<iostream>
+        using namespace std;
+
+        int main(){
+ 
+    
+            int number = 0 ;
+            cout<<"ender the number which you need to double"<<endl;
+           
+            int a[10]; 
+            
+            for(int i = 1 ; i<10 ;i++){
+         
+                        cin>>a[i];
+                         
+                     
+             
+            }
+             for(int i = 1 ; i<10 ;i++){
+         
+                       cout<<"printing the number: "<<a[i]<<endl;
+                         
+           
+             
+            }
+             for(int i = 1 ; i<10 ;i++){
+                              a[i] = a[i]*2;
+                       cout<<"printing the double value: "<<a[i]<<endl;
+                         
+           
+             
+            }
+
+
+
+
+
+        }
