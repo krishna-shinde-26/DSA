72f1c32ee360a13b21b4594021f4eeed74ebf3e4
krishna-shinde-26 <krishnashindeoffcial@gmail.com>
2026-01-26 16:29:37 +0530

    assignment 1 done

--- CPP/Arrary /assignment_1.cpp
+++ CPP/Arrary /assignment_1.cpp
@@ -36,3 +36,29 @@
 
 
         }
+/*
+Output 
+    ender the number which you need to double
+    1 2 3 4 5 6 7 8 9
+    printing the number: 1
+    printing the number: 2
+    printing the number: 3
+    printing the number: 4
+    printing the number: 5
+    printing the number: 6
+    printing the number: 7
+    printing the number: 8
+    printing the number: 9
+    printing the double value: 2
+    printing the double value: 4
+    printing the double value: 6
+    printing the double value: 8
+    printing the double value: 10
+    printing the double value: 12
+    printing the double value: 14
+    printing the double value: 16
+    printing the double value: 18
+    
+
+=== Code Execution Successful ===
+*/
