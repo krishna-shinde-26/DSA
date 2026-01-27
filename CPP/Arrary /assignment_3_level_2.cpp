fcbe2edc5cbc089e6d6df5a89e3f97f16e6f1643
krishna-shinde-26 <krishnashindeoffcial@gmail.com>
2026-01-27 21:00:04 +0530

    Assignment 3 level 2

---
+++ CPP/Arrary /assignment_3_level_2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void Pair(int arr[],int size){
+  
+    int i = 0;
+    int ans = 0;
+     
+    for(i=0 ;i<size;i++){
+                       
+          for(int j = 0;j<size;j++){
+
+                                          ;
+
+             for(int k = 0;k <size;k++){
+
+               cout<<arr[i]<<" : "<<arr[j]<<" : "<<arr[k]<<endl                                     ;
+
+
+          }
+          }
+             
+
+         }
+        
+
+        }
+
+
+int main(){
+   
+    int size = 3;
+    int arr[size] =  {10,20,30};
+     Pair(arr,size);
+     
+    
+}
