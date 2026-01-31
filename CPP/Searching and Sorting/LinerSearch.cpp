5710498c94e51082f1137a14fb7f1da88393b933
krishna-shinde-26 <krishnashindeoffcial@gmail.com>
2026-01-31 22:43:23 +0530

    Liner Search

---
+++ CPP/Searching and Sorting/LinerSearch.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std;
+
+int linerSerach(int arr[],int&size,int&target){
+        
+          for(int i= 0;i<size-1;i++){
+
+            if(arr[i] == target){
+
+               return i;
+
+            }
+      
+ 
+             
+
+       }
+return -1;
+
+
+
+    }
+
+
+int main(){
+
+    int arr[] = {1,2,3,4,5};
+    int size = 5 ;
+          int target = 4;
+      int index = linerSerach(arr,size,target);
+        if(index==-1){
+
+            cout<<"there is a no element found like that "<<endl;
+            
+            
+        }
+        
+        else{
+            
+            cout<<"i have found the element "<< index;
+
+        }
+
+}
